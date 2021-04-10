2d65f5161da5b6c7a214e884674f75fb113e27d3
ZhikaiWu2021 <zhikaiw1@student.unimelb.edu.au>
2021-04-10 15:49:28 +0800

    challengeviewing.css

---
+++ challengeviewing-page/candy.js
@@ -0,0 +1,10 @@
+var num_candy = 1000;
+
+function gain_candy() {
+    num_candy = num_candy + 10; 
+}
+
+let challenge_content;
+challenge_content = 'runing asdasd, content asdasdasdasdsa';
+
+var due_date = '11/04/2021'
