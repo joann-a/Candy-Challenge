a05cdb32de325164c8220a20f46cc561358d327c
Bruce Zhu <brucezzhu@gmail.com>
2021-04-11 11:58:22 +1000

    deployed

---
+++ public/login.js
@@ -0,0 +1,135 @@
+
+
+
+function setFormMessage(formElement, type, message) {
+    const messageElement = formElement.querySelector(".form__message");
+
+    messageElement.textContent = message;
+    messageElement.classList.remove("form__message--success", "form__message--error");
+    messageElement.classList.add(`form__message--${type}`);
+}
+function clearFormMessage(message) {
+    messageElement.classList.remove(message);
+    
+}
+
+
+// function setInputError(inputElement, message) {
+//     inputElement.classList.add("form__input--error");
+//     inputElement.parentElement.querySelector(".form__input-error-message").textContent = message;
+// }
+
+function clearInputError(inputElement) {
+    inputElement.classList.remove("form__input--error");
+    inputElement.parentElement.querySelector(".form__input-error-message").textContent = "";
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    const loginForm = document.querySelector("#login");
+    const createAccountForm = document.querySelector("#createAccount");
+
+    document.querySelector("#linkCreateAccount").addEventListener("click", e => {
+        e.preventDefault();
+        loginForm.classList.add("form--hidden");
+        createAccountForm.classList.remove("form--hidden");
+    });
+
+    document.querySelector("#linkLogin").addEventListener("click", e => {
+        e.preventDefault();
+        loginForm.classList.remove("form--hidden");
+        createAccountForm.classList.add("form--hidden");
+    });
+
+    // document.querySelectorAll(".form__input").forEach(inputElement => {
+    //     inputElement.addEventListener("blur", e => {
+    //         if (e.target.id === "signupPassword" && e.target.value.length > 0 && e.target.value.length < 6) {
+    //             setInputError(inputElement, "Password must be at least  characters in length");
+    //         }
+    //     });
+
+    //     inputElement.addEventListener("input", e => {
+    //         clearInputError(inputElement);
+    //     });
+    // });
+
+   
+    // var database = firebase.database();
+    // function writeUserData(name, email) {
+    //     firebase.database().ref('Users/' + name).set({
+    //         username: name,
+    //         email: email,
+    //     }, (error) => {
+    //         if (error) {
+    //             alert(error.message);
+    //         } else {
+    //             // Data saved successfully!
+    //         }   
+    //     });
+    // }
+
+    createAccountForm.addEventListener("submit", e => {
+        e.preventDefault();
+        // sign up
+
+        var name = document.getElementById("signupUsername").value;
+        var email = document.getElementById("signupEmail").value;
+        var password = document.getElementById("signupPassword").value;
+        
+        
+        firebase.auth().createUserWithEmailAndPassword(email, password)
+        .then((userCredential) => {
+            // Sign up
+            var user = userCredential.user;
+            // ...
+            alert("Signed up successfully");           
+            
+            
+        })
+        .catch((error) => {
+            var errorCode = error.code;
+            var errorMessage = error.message;
+            
+            alert(errorMessage);
+        });
+
+        
+        
+        
+        
+        
+        
+    });
+
+    loginForm.addEventListener("submit", e => {
+        e.preventDefault();
+
+        // login
+        var email = document.getElementById("loginEmail").value;
+        var password = document.getElementById("loginPassword").value;
+
+        firebase.auth().signInWithEmailAndPassword(email, password)
+            .then((userCredential) => {
+                // Signed in
+                var user = userCredential.user;
+                // ...
+                alert("Welcome " + email);
+                window.location = "home.html";
+                
+            })
+            .catch((error) => {
+                var errorCode = error.code;
+                var errorMessage = error.message;
+                setFormMessage(loginForm, "error", error.message);
+                
+            });
+            setFormMessage(loginForm, "error", "");
+        
+        
+    });
+
+    
+});
+
+
+
+
